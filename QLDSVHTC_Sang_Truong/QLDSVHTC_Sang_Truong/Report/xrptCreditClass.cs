327e674a6e0c9e578f5e3d56af8993362048c425
huynhphuocsang <huynhphuocsang23@gmail.com>
2021-10-09 20:32:11 +0700

    update report credit class

---
+++ QLDSVHTC_Sang_Truong/QLDSVHTC_Sang_Truong/Report/xrptCreditClass.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace QLDSVHTC_Sang_Truong.Report
+{
+    public partial class xrptCreditClass : DevExpress.XtraReports.UI.XtraReport
+    {
+        public xrptCreditClass()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
